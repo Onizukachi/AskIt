2dfd5927e4423a87694153597a85d2c06f5a73e0
onizuka <dark_sao@mail.ru>
2022-08-07 21:47:26 +0300

    add kaminari and paginator

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,7 +3,7 @@ class QuestionsController < ApplicationController
 
   def show
     @answer = @question.answers.build
-    @answers = Answer.order created_at: :desc
+    @answers = Answer.where(question: @question).order(created_at: :desc).page(params[:page])
   end
 
   def destroy
@@ -25,7 +25,7 @@ class QuestionsController < ApplicationController
   end
 
   def index
-    @questions = Question.all
+    @questions = Question.order(created_at: :desc).page params[:page]
   end
 
   def new
