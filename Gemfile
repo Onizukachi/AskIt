a7ee879163377c5c4a15f92f83263cdfc3c12ca8
onizuka <dark_sao@mail.ru>
2022-08-09 13:48:06 +0300

    create registration and model Session

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ gem 'bcrypt', '~> 3.1.7'
 
 gem 'pagy', '~> 5.10'
 gem 'draper', '~> 4.0.2'
+gem "valid_email2", '~> 4.0.3'
 
 # Use Active Storage variant
 # gem 'image_processing', '~> 1.2'
--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -11,6 +11,27 @@ module Authentication
     def user_signed_in?
       current_user.present?
     end
+
+    def require_no_authentication
+      return unless user_signed_in?
+      flash[:warning] = "You are already signed in!"
+      redirect_to root_path
+    end
+
+    def require_authentication
+      return if user_signed_in?
+
+      flash[:warning] = "You are are not signed in!"
+      redirect_to root_path
+    end
+
+    def sign_in(user)
+      session[:user_id] = user.id
+    end
+
+    def sign_out
+      session.delete :user_id
+    end
   
     helper_method :current_user, :user_signed_in?
   end
